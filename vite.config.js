0afae0c396b4ba51412e86e3b098e05efc21b015
romarin-hsieh <141517848+romarin-hsieh@users.noreply.github.com>
2026-02-06 16:01:58 +0800

    Fix indicators with proxy

--- vite.config.js
+++ vite.config.js
@@ -48,7 +48,8 @@ export default defineConfig({
         secure: false,
         headers: {
           'Origin': 'https://finance.yahoo.com',
-          'Referer': 'https://finance.yahoo.com'
+          'Referer': 'https://finance.yahoo.com',
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
         }
       }
     }
